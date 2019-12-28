effb92ca0104f4da1a8bfc33e7295ad6b5462524
Sokol Dmitrii <sokol.dmitry.f@gmail.com>
2019-12-28 21:38:49 +0300

    Add context generation for interactive Python shell

--- app.py
+++ app.py
@@ -2,6 +2,14 @@
 
 from adverts import app
 
+@app.shell_context_processor
+def make_shell_context():
+    import adverts
+    import adverts.models
+    context = vars(adverts)
+    context.update(vars(adverts.models))
+    return context
+
 
 if __name__ == '__main__':
     app.run(debug=True)
