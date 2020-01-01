5456585dc2eb2acd6c2b98f587cf8fa92bec40e7
Sokol Dmitrii <sokol.dmitry.f@gmail.com>
2020-01-01 18:26:05 +0300

    Implemented endpoint for getting one advertisement

--- adverts/routes.py
+++ adverts/routes.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import request, jsonify
 
 from . import app
 from . import services
@@ -6,8 +6,10 @@ from . import serializers
 
 @app.route('/advertisement/<int:advert_id>', methods=['GET'])
 def get_advert_item(advert_id):
-    
-    return 'ok'
+    return jsonify(serializers.advert_to_json(
+        services.get_advertisement(advert_id),
+        request.args.get('fields', '')
+    ))
 
 
 @app.route('/advertisement', methods=['GET'])
