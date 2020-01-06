64726c98e27fde9cfb155c184b7788851f70c110
Sokol Dmitrii <sokol.dmitry.f@gmail.com>
2020-01-06 16:49:35 +0300

    Add field 'id' into model the server returns.

--- adverts/serializers.py
+++ adverts/serializers.py
@@ -28,6 +28,7 @@ def photo_to_json(photo):
 def advert_to_json(advert, fields=''):
     fields = fields.split(',')
     json = {
+        'id': advert.advert_id,
         'title': advert.title,
         'price': advert.price,
         'main_photo': photo_to_json(advert.main_photo),
